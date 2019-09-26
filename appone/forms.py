6bcac3f108b8bdd082291c46d34a51d25f8fd850
Your Name <you@example.com>
2019-09-27 00:50:25 +0200

    add placeholder in form

--- appone/forms.py
+++ appone/forms.py
@@ -2,13 +2,16 @@ from django import forms
 
 
 class TestForm(forms.Form):
-    title = forms.CharField(label="Enter a title", max_length=50)
-    text = forms.CharField(label="Enter your text",
-                           widget=forms.Textarea,
+    title = forms.CharField(label="",
+                            widget=forms.TextInput(attrs={'placeholder': 'Enter a title'}),
+                            max_length=50)
+
+    text = forms.CharField(label="",
+                           widget=forms.Textarea(attrs={'placeholder': 'Enter your text'}),
                            max_length=10000)
 
 
 class LeaveComment(forms.Form):
-    content = forms.CharField(label="Leave a comment",
-                              widget=forms.Textarea,
+    content = forms.CharField(label="",
+                              widget=forms.Textarea(attrs={'placeholder': 'Leave a comment'}),
                               max_length=1000)
