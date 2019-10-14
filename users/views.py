ad30bd16e840897e77b5e647ba7dd15c5391b3b5
Your Name <you@example.com>
2019-10-14 23:46:47 +0200

    remove format

--- users/views.py
+++ users/views.py
@@ -35,7 +35,7 @@ def profile(request):
         if u_form.is_valid():
             u_form.save()
 
-            messages.success(request, f'Your account has been updated!')
+            messages.success(request, 'Your account has been updated!')
             return redirect('home')
 
     else:
